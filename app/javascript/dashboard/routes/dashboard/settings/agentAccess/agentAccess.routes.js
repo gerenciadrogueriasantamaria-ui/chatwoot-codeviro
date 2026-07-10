ba6fc412db73e64bbc380ab52d6670204a6a7f48
gerenciadrogueriasantamaria-ui <gerencia.drogueriasantamaria@gmail.com>
2026-07-09 22:40:08 -0500

    Refactor agent access route to use shared meta permissions

--- app/javascript/dashboard/routes/dashboard/settings/agentAccess/agentAccess.routes.js
+++ app/javascript/dashboard/routes/dashboard/settings/agentAccess/agentAccess.routes.js
@@ -2,19 +2,22 @@ import { frontendURL } from '../../../../helper/URLHelper';
 import SettingsWrapper from '../SettingsWrapper.vue';
 import Index from './Index.vue';
 
+const meta = {
+  permissions: ['administrator'],
+};
+
 export default {
   routes: [
     {
       path: frontendURL('accounts/:accountId/settings/agent-access'),
       component: SettingsWrapper,
+      meta,
       children: [
         {
           path: '',
           name: 'agent_access_settings_index',
           component: Index,
-          meta: {
-            permissions: ['administrator'],
-          },
+          meta,
         },
       ],
     },
