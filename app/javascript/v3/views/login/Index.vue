1c462a25ca6d9765dd635ff220047748ea1396c3
gerenciadrogueriasantamaria-ui <gerencia.drogueriasantamaria@gmail.com>
2026-07-30 23:20:02 -0500

    Update logo image classes for responsive design

--- app/javascript/v3/views/login/Index.vue
+++ app/javascript/v3/views/login/Index.vue
@@ -236,13 +236,13 @@ export default {
       <img
         :src="globalConfig.logo"
         :alt="globalConfig.installationName"
-        class="block w-auto h-8 mx-auto dark:hidden"
+        class="block h-auto w-[18.5rem] max-w-[70vw] mx-auto dark:hidden"
       />
       <img
         v-if="globalConfig.logoDark"
         :src="globalConfig.logoDark"
         :alt="globalConfig.installationName"
-        class="hidden w-auto h-8 mx-auto dark:block"
+        class="hidden h-auto w-[18.5rem] max-w-[70vw] mx-auto dark:block"
       />
       <h2 class="mt-6 text-3xl font-medium text-center text-n-slate-12">
         {{ replaceInstallationName($t('LOGIN.TITLE')) }}
