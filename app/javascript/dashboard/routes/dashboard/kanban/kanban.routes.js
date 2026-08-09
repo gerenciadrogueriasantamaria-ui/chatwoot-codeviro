d32f40a9ef2441a74ff09249e895af8b28988680
gerenciadrogueriasantamaria-ui <gerencia.drogueriasantamaria@gmail.com>
2026-08-08 20:48:33 -0500

    Add Kanban route with permissions for administrators

--- app/javascript/dashboard/routes/dashboard/kanban/kanban.routes.js
+++ app/javascript/dashboard/routes/dashboard/kanban/kanban.routes.js
@@ -1,1 +1,18 @@
+import { frontendURL } from 'dashboard/helper/URLHelper';
 
+import Kanban from './Kanban.vue';
+
+export const routes = [
+  {
+    path: frontendURL('accounts/:accountId/kanban'),
+    name: 'kanban',
+    meta: {
+      permissions: ['administrator'],
+    },
+    component: Kanban,
+  },
+];
+
+export default {
+  routes,
+};
